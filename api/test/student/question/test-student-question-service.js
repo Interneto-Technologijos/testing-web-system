841a0187fe226fb410193c79278dcfe62f90cbca
Mykolas <mykolas.molis@vgtu.lt>
2021-03-18 22:24:49 +0200

    Fix answer randomization

--- api/test/student/question/test-student-question-service.js
+++ api/test/student/question/test-student-question-service.js
@@ -1,23 +1,37 @@
-const questionRepository = require('../../../question/question-repository');
-const testStudentQuestionRepository = require('./test-student-question-repository');
+const questionRepository = require("../../../question/question-repository");
+const testStudentQuestionRepository = require("./test-student-question-repository");
 
-const createTestStudentQuestions = async (testId, studentId) => ({
+const createTestStudentQuestions = async (testId, studentId) => {
+  return {
     testId,
     studentId,
-    questions: (await questionRepository.findRandom(20)).map((question, index) => ({
-        id: index + 1,
-        questionId: question._id,
-        question: question.question,
-        options: [...question.incorrectOptions, question.corectOption],
-    })),
-});
+    questions: (await questionRepository.findRandom(20)).map(
+      (question, index) => {
+        question.incorrectOptions.splice(
+          Math.floor(Math.random() * 4),
+          0,
+          question.corectOption
+        );
+        return {
+          id: index + 1,
+          questionId: question._id,
+          question: question.question,
+          options: question.incorrectOptions,
+        };
+      }
+    ),
+  };
+};
 
 module.exports.getQuestionsByTestIdAndStudentId = async (testId, studentId) => {
-    const existingQuestions = await testStudentQuestionRepository.findByTestIdAndStudentId(testId, studentId);
-    if (existingQuestions) {
-        return existingQuestions; 
-    }
+  const existingQuestions = await testStudentQuestionRepository.findByTestIdAndStudentId(
+    testId,
+    studentId
+  );
+  if (existingQuestions) {
+    return existingQuestions;
+  }
 
-    const questions = await createTestStudentQuestions(testId, studentId);
-    return testStudentQuestionRepository.save(questions);
+  const questions = await createTestStudentQuestions(testId, studentId);
+  return testStudentQuestionRepository.save(questions);
 };
